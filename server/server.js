d3ad497b385fcc9499a4e5f4ff14812e46d542fa
Starmoon9159 <qwer091121@yahoo.com>
2023-09-19 21:12:32 +0800

    Updata DashDoard

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const port = '5000';
 const app = express();
+const maps = require('./configs/maps.json')
 
-app.get("/api",(req,res) =>{
-    res.json({users:["userOne","userTwo","userThree"]})
-})
-
-app.listen(port,() =>{
+app.listen(port, () => {
     console.log(`Sever started on port ${port}`)
 })
+
+app.get("/api", (req, res) => {
+    let maps_count = Object.keys(maps).length;
+    res.json({
+        "Maps": maps,
+        "count":maps_count
+    })
+})
+
